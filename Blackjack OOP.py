87b3cf31a9b7cc208e82eb6346d6017ba115844a
Alexander Large <alexanderklarge@gmail.com>
2019-08-16 11:59:04 +0100

    Made a branch where I use class inheritence, so I made a class called Card, and each suit is a class which inherits features from Card. Much cleaner!!

--- Blackjack OOP.py
+++ Blackjack OOP.py
@@ -7,9 +7,7 @@
 
 import numpy as np
 
-class Club:
-    suit = 'Club' # this is from tutorial 2 - it's a class variable!
-
+class Card:
     def __init__(self, name, value):
         self.name = name
         self.value = value
@@ -17,6 +15,22 @@ class Club:
     # Creating a method within the class!
     def display_card(self):
         return '{} of {}s'.format(self.name, self.suit)
+
+
+class Club(Card): # putting (Card) in the brackets means this class will inherit the features of the Card class!
+    suit = 'Club' # this is from tutorial 2 - it's a class variable!
+
+
+class Spade(Card):
+    suit = 'Spade' # this is from tutorial 2 - it's a class variable!
+
+
+class Heart(Card):
+    suit = 'Heart' # this is from tutorial 2 - it's a class variable!
+
+
+class Diamond(Card):
+    suit = 'Diamond' # this is from tutorial 2 - it's a class variable!
 
 card1 = Club('Ace', '1 or 11')
 card2 = Club(2, 2)
@@ -32,17 +46,6 @@ card11 = Club('Jack', 10)
 card12 = Club('Queen', 10)
 card13 = Club('King', 10)
 
-class Spade:
-    suit = 'Spade' # this is from tutorial 2 - it's a class variable!
-
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
-    # Creating a method within the class!
-    def display_card(self):
-        return '{} of {}s'.format(self.name, self.suit)
-
 card14 = Spade('Ace', '1 or 11')
 card15 = Spade(2, 2)
 card16 = Spade(3, 3)
@@ -57,17 +60,6 @@ card24 = Spade('Jack', 10)
 card25 = Spade('Queen', 10)
 card26 = Spade('King', 10)
 
-class Heart:
-    suit = 'Heart' # this is from tutorial 2 - it's a class variable!
-
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
-    # Creating a method within the class!
-    def display_card(self):
-        return '{} of {}s'.format(self.name, self.suit)
-
 card27 = Heart('Ace', '1 or 11')
 card28 = Heart(2, 2)
 card29 = Heart(3, 3)
@@ -81,17 +73,6 @@ card36 = Heart(1, 10)
 card37 = Heart('Jack', 10)
 card38 = Heart('Queen', 10)
 card39 = Heart('King', 10)
-
-class Diamond:
-    suit = 'Diamond' # this is from tutorial 2 - it's a class variable!
-
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
-    # Creating a method within the class!
-    def display_card(self):
-        return '{} of {}s'.format(self.name, self.suit)
 
 card40 = Diamond('Ace', '1 or 11')
 card41 = Diamond(2, 2)
@@ -129,4 +110,5 @@ print('Random card 2 is {}'.format(random_card_2.display_card()))
 # i.e. it tells you your hand value, and the dealer has 2 secret cards, and then asks you to hit or stick
 # But I thought I'd leave it here for now
 # You guys could even add that functionality here!
-# Would be interested to see how you'd go about it 
+# Would be interested to see how you'd go about it
+
